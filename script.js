7131178b2128c0fd83e4498eedfc96f4fe671d60
Firdaus Deni Febriansyah <36836741+FirdausDeni@users.noreply.github.com>
2025-07-10 14:18:51 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const contactForm = document.getElementById('contactForm');
+
+    if (contactForm) {
+        contactForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            const name = document.getElementById('name').value;
+            const message = document.getElementById('message').value;
+            const whatsappNumber = document.getElementById('whatsappNumber').value; 
+
+            const targetWhatsApp = '6281367914366'; // Nomor WhatsApp tujuan 
+
+            // Ini adalah bagian yang menentukan isi pesan WhatsApp.
+            // Hanya mengambil teks dari kolom 'Pesan Anda'.
+            let whatsappMessage = `${message}`;
+
+            const encodedMessage = encodeURIComponent(whatsappMessage);
+            const whatsappURL = `https://wa.me/${targetWhatsApp}?text=${encodedMessage}`;
+
+            window.open(whatsappURL, '_blank');
+        });
+    }
+});
